3f59f42c342906b4709e55a43e9f7cbe7872ab43
Clayton Ribeiro <clayton.ribeiro@5by5.com.br>
2021-11-14 18:28:41 -0300

    adicionando rotas e primeiras migrations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
-
+import appointmentsRouter from './appointments.routes';
 const routes = Router();
+
+routes.use('/appointment', appointmentsRouter);
 
 routes.get('/', (request, response) =>
   response.json({ message: 'Hello World2' }),
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import routes from './routes';
+import 'reflect-metadata';
+
+import './database';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
