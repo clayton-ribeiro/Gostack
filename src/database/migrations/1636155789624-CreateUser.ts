910fc2720e32b62c0d187ee9bf9a10d4ce90bbe0
Clayton Ribeiro <clayton.ribeiro@5by5.com.br>
2021-11-15 19:04:12 -0300

    adicionando classes de usuario e validações na criação do mesmo

--- src/database/migrations/1636155789624-CreateUser.ts
+++ src/database/migrations/1636155789624-CreateUser.ts
@@ -8,7 +8,7 @@ export default class CreateUser1636155789624 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1636927762546-CreateAppointments.ts
+++ src/database/migrations/1636927762546-CreateAppointments.ts
@@ -8,7 +8,7 @@ export class CreateAppointments1636927762546 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
